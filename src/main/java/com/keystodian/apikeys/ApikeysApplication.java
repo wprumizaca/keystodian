d54b0358ba2d0090210edf6442fd1699e99e0286
wrafa <wperezrumizaca@gmail.com>
2023-05-15 11:36:16 +0200

    Aplcacion inicial

---
+++ src/main/java/com/keystodian/apikeys/ApikeysApplication.java
@@ -0,0 +1,13 @@
+package com.keystodian.apikeys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApikeysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApikeysApplication.class, args);
+	}
+
+}
