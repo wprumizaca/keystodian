1355a5d07e66d297aa3db46420a57d09d3167ae1
wprumizaca <wperezrumizaca@gmail.com>
2023-05-19 11:26:43 +0200

    Modifico User y Añado dependencias Seguridad y validacion(notblank)

--- src/main/java/com/keystodian/apikeys/persistence/entities/User.java
+++ src/main/java/com/keystodian/apikeys/persistence/entities/User.java
@@ -1,18 +1,66 @@
 package com.keystodian.apikeys.persistence.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
 
-@Getter @Setter
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
-    @Id
-    private String id;
-    private String user_password;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;
+
+
+    @Column(unique = true)
+    private String username;
+
+
+    private String email;
+
+    private String password;
+
+
+    @ElementCollection(fetch = FetchType.EAGER) //Relacionamos con UserRole
+    @Enumerated(EnumType.STRING) //la enumeracion de roles se almacenará en BBDD como string
+    private Set<UserRole> roles;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { //nos devuelve los roles/permisos del usuario
+        return roles
+                .stream()
+                .map(role-> new SimpleGrantedAuthority("ROLE_"+role.toString()))//Permisos que tendrá. No confundir ROL con AUTHORITIES
+                .toList();
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ src/main/java/com/keystodian/apikeys/persistence/entities/UserRole.java
@@ -0,0 +1,7 @@
+package com.keystodian.apikeys.persistence.entities;
+
+public enum UserRole {
+    MASTER, ADMIN_USER, ADMIN_APP
+
+
+}
