218c9caccb7c610d822638591214c2cc97d6cd6d
wrafa <wperezrumizaca@gmail.com>
2023-05-24 23:47:40 +0200

    Completo UserController. PasswordController en blanco

--- src/main/java/com/keystodian/apikeys/expose/controllers/PasswordController.java
+++ src/main/java/com/keystodian/apikeys/expose/controllers/PasswordController.java
@@ -1,4 +1,9 @@
 package com.keystodian.apikeys.expose.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class PasswordController {
 }
--- src/main/java/com/keystodian/apikeys/expose/controllers/UserController.java
+++ src/main/java/com/keystodian/apikeys/expose/controllers/UserController.java
@@ -1,13 +1,63 @@
 package com.keystodian.apikeys.expose.controllers;
 
 
+import com.keystodian.apikeys.exceptions.IdUserNotFoundException;
+import com.keystodian.apikeys.expose.dto.dtoCambios.UpdateUserEmailDTO;
+import com.keystodian.apikeys.expose.dto.dtoCambios.UpdateUserPasswDTO;
+import com.keystodian.apikeys.expose.dto.dtoUser.UserResponse;
+import com.keystodian.apikeys.persistence.entities.User;
+import com.keystodian.apikeys.persistence.repository.UserRepository;
+import com.keystodian.apikeys.services.impl.UserService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+    private final UserRepository userRepository;
+
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAll(){
+        return new ResponseEntity<List<UserResponse>>(userService.findAll(), HttpStatus.OK);
+    }
+
+    //Para comprobar lo que se guarda en ENTIDAD
+    @GetMapping("/us/{id}")
+    public ResponseEntity<User>find(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(userRepository.findById(id)
+                .orElseThrow(() -> new IdUserNotFoundException(id)));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse>findById(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
+    }
+
+
+
+    @PutMapping("/email/{id}")
+    public ResponseEntity<UserResponse> updateEmail(@RequestBody UpdateUserEmailDTO updateUserEmailDTO, @PathVariable Long id){
+        return new ResponseEntity<UserResponse>(userService.editEmail(updateUserEmailDTO,id), HttpStatus.OK);
+    }
+
+    @PutMapping("/password/{id}")
+    public ResponseEntity<UserResponse> updatePassword(@RequestBody UpdateUserPasswDTO updateUserPasswDTO , @PathVariable Long id){
+        return new ResponseEntity<UserResponse>(userService.editPassw(updateUserPasswDTO,id), HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        userService.deleteById(id);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
 
 }
